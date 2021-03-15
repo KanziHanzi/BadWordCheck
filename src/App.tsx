ccdca643938b8aebd5b51b6068c54ea3eec60d7f
Janik Grigutsch <janik.grigutsch@i22.de>
2021-03-15 09:42:22 +0100

    feat(textbox): add fetch method to check string input

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
-import React from 'react';
-import './scss/style.scss'
+import React, { useState } from "react";
+import "./scss/style.scss";
+
+const fetchData = (message: string) => {
+  fetch(
+    `https://nethergames.de:1763/check?message=${message}&render=true`
+  ).then((res) => {
+    console.log(res);
+  });
+};
 
 const App = () => {
+  const [textAreaValue, setTextAreaValue] = useState<string>("");
   return (
     <div className="container">
-      <div className="container__text"></div>
+      <div className="container__text">
+        <textarea
+          value={textAreaValue}
+          onChange={(ev: React.ChangeEvent<HTMLTextAreaElement>): void =>
+            setTextAreaValue(ev.target.value)
+          }
+          className="container__input"
+        />
+      </div>
+      <div
+        onClick={() => {
+          fetchData(textAreaValue);
+        }}
+        className="container__button"
+      >
+        Button
+      </div>
     </div>
   );
-}
+};
 
 export default App;
