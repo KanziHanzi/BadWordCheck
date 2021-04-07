f8685ecb3716152217aa7e1d9a98f45892c2959e
Janik Grigutsch <janik.grigutsch@i22.de>
2021-04-07 17:48:32 +0200

    feat: add badWordScore & minor tweaks

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import "./scss/style.scss";
-
-const outputEl = React.createRef<HTMLDivElement>();
+import { scoreColors } from './utils/scoreColors.js';
 
 const fetchData = (message: string) => {
   const http = new XMLHttpRequest();
@@ -12,6 +11,7 @@ const fetchData = (message: string) => {
     outputEl.current!.innerHTML = http.responseText;
     if (http.readyState == 4) {
       listBadWords();
+      badWordScore(listBadWords());
     }
   };
 };
@@ -21,13 +21,60 @@ const showErrorMessage = (error: string) => {
 };
 
 const listBadWords = () => {
-  const badWordList = document.getElementsByTagName("b");
+  const badWordNodes = document.getElementsByTagName("b");
   const badWordArr = [];
   let i: number = 0;
-  for (i; i < badWordList.length; i++) {
-    badWordArr.push(badWordList[i].innerHTML);
+  for (i; i < badWordNodes.length; i++) {
+    badWordArr.push(badWordNodes[i].innerHTML);
   }
+  return badWordArr;
 };
+
+const badWordScore = (badWords: any) => {
+  const allWords = outputEl.current!.innerText;
+  const allWordsArray = allWords.split(/\s+/);
+  const badWordsCount = badWords.length;
+  const allWordsCount = allWordsArray.length;
+
+  const score = badWordsCount / allWordsCount;
+  const formattedScore = Math.round(score*100)/100;
+
+  console.log(formattedScore);
+
+  switch (formattedScore !== undefined && formattedScore !== null) {
+    case formattedScore === 0:
+      scoreEl.current!.style.borderColor = scoreColors.perfect;
+      break;
+    case formattedScore > 0 && formattedScore <= 0.2:
+      console.log('light');
+      scoreEl.current!.style.borderColor = scoreColors.light;
+      break;
+    case formattedScore > 0.2 && formattedScore <= 0.4:
+      console.log('medium');
+      scoreEl.current!.style.borderColor = scoreColors.medium;
+      break;
+    case formattedScore > 0.4 && formattedScore <= 0.6:
+      console.log('high');
+      scoreEl.current!.style.borderColor = scoreColors.high;
+      break;
+    case formattedScore > 0.6 && formattedScore <= 0.8:
+      console.log('veryHigh');
+      scoreEl.current!.style.borderColor = scoreColors.veryHigh;
+      break;
+    case formattedScore > 0.8 && formattedScore < 1:
+      console.log('extreme');
+      scoreEl.current!.style.borderColor = scoreColors.extreme;
+      break;
+    case formattedScore === 1:
+      console.log('not safe for work');
+      scoreEl.current!.style.borderColor = scoreColors.notSafeForWork;
+      break;
+    }
+  scoreEl.current!.innerHTML = formattedScore.toString();
+};
+
+const outputEl = React.createRef<HTMLDivElement>();
+const scoreEl = React.createRef<HTMLDivElement>();
 
 const App = () => {
   const [textAreaValue, setTextAreaValue] = useState<string>("");
@@ -53,6 +100,7 @@ const App = () => {
           Check String
         </button>
         <div className="wordcheck__output" ref={outputEl} />
+        <div className="wordcheck__score" ref={scoreEl} />
       </div>
     </div>
   );
---
+++ frontend/src/utils/scoreColors.js
@@ -0,0 +1,9 @@
+export const scoreColors = {
+  perfect: '#34eb34',
+  light: '#ebe234',
+  medium: '#ebb134',
+  high: '#eb7734',
+  veryHigh: '#eb4934',
+  extreme: '#bd0404',
+  notSafeForWork: '#850303'
+}
