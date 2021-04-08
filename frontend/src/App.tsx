a0eb869826deeed17940f0178dc5209967c20216
Janik Grigutsch <janik.grigutsch@i22.de>
2021-04-08 13:34:02 +0200

    feat: change app functional component to class component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { ReactNode } from "react";
 import "./scss/style.scss";
-import { scoreColors } from './utils/scoreColors.js';
+import { scoreColors } from "./utils/scoreColors.js";
 
 const fetchData = (message: string) => {
   const http = new XMLHttpRequest();
@@ -9,7 +9,7 @@ const fetchData = (message: string) => {
   http.send();
   http.onreadystatechange = () => {
     outputEl.current!.innerHTML = http.responseText;
-    if (http.readyState == 4) {
+    if (http.readyState === 4) {
       listBadWords();
       badWordScore(listBadWords());
     }
@@ -37,72 +37,78 @@ const badWordScore = (badWords: any) => {
   const allWordsCount = allWordsArray.length;
 
   const score = badWordsCount / allWordsCount;
-  const trimmedScore = Math.round(score*100)/100;
-  const formattedScore = trimmedScore * 100 + '%'
+  const trimmedScore = Math.round(score * 100) / 100;
+  const formattedScore = trimmedScore * 100 + "%";
 
-  switch (trimmedScore !== undefined && trimmedScore !== null) {
+  switch (allWords !== "") {
     case trimmedScore === 0:
       scoreEl.current!.style.borderColor = scoreColors.perfect;
       break;
     case trimmedScore > 0 && trimmedScore <= 0.2:
-      console.log('light');
       scoreEl.current!.style.borderColor = scoreColors.light;
       break;
     case trimmedScore > 0.2 && trimmedScore <= 0.4:
-      console.log('medium');
       scoreEl.current!.style.borderColor = scoreColors.medium;
       break;
     case trimmedScore > 0.4 && trimmedScore <= 0.6:
-      console.log('high');
       scoreEl.current!.style.borderColor = scoreColors.high;
       break;
     case trimmedScore > 0.6 && trimmedScore <= 0.8:
-      console.log('veryHigh');
       scoreEl.current!.style.borderColor = scoreColors.veryHigh;
       break;
     case trimmedScore > 0.8 && trimmedScore < 1:
-      console.log('extreme');
       scoreEl.current!.style.borderColor = scoreColors.extreme;
       break;
     case trimmedScore === 1:
-      console.log('not safe for work');
       scoreEl.current!.style.borderColor = scoreColors.notSafeForWork;
       break;
-    }
+  }
   scoreEl.current!.innerHTML = formattedScore;
 };
 
 const outputEl = React.createRef<HTMLDivElement>();
 const scoreEl = React.createRef<HTMLDivElement>();
 
-const App = () => {
-  const [textAreaValue, setTextAreaValue] = useState<string>("");
-  return (
-    <div className="wordcheck">
-      <div className="wordcheck__container">
-        <textarea
-          placeholder="Hier Text eingeben"
-          value={textAreaValue}
-          onChange={(event: React.ChangeEvent<HTMLTextAreaElement>): void =>
-            setTextAreaValue(event.currentTarget.value)
-          }
-          className="wordcheck__textarea"
-        />
-        <button
-          onClick={async () => {
-            textAreaValue !== ""
-              ? fetchData(textAreaValue)
-              : showErrorMessage("Bitte füge einen Text ein");
-          }}
-          className="wordcheck__button"
-        >
-          Check String
-        </button>
-        <div className="wordcheck__output" ref={outputEl} />
-        <div className="wordcheck__score" ref={scoreEl} />
+class App extends React.Component<{}, { textAreaValue: string }> {
+  constructor(props: any) {
+    super(props);
+    this.state = { textAreaValue: "" };
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  handleInput(event: any) {
+    this.setState(() => {
+      return { textAreaValue: (event.target as HTMLTextAreaElement).value };
+    });
+  }
+
+  render(): ReactNode {
+    return (
+      <div className="wordcheck">
+        <div className="wordcheck__container">
+          <textarea
+            placeholder="Hier Text eingeben"
+            onKeyPress={() => console.log("test")}
+            value={this.state.textAreaValue}
+            onChange={this.handleInput}
+            className="wordcheck__textarea"
+          />
+          <button
+            onClick={async () => {
+              this.state.textAreaValue !== ""
+                ? fetchData(this.state.textAreaValue)
+                : showErrorMessage("Bitte füge einen Text ein");
+            }}
+            className="wordcheck__button"
+          >
+            Check String
+          </button>
+          <div className="wordcheck__output" ref={outputEl} />
+          <div className="wordcheck__score" ref={scoreEl} />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
